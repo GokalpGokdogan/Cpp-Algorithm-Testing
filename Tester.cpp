17895676fb494f2a51bfecc58b2a54eaa6edd132
Gokalp <gokalp.gokdogan@ug.bilkent.edu.tr>
2023-01-30 13:12:03 +0300

    working version of Merge Two Sorted Lists

--- Tester.cpp
+++ Tester.cpp
@@ -16,8 +16,8 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
 
 int main() {
 
-    vector<int> v1 = { 1,2,4};
-    vector<int> v2 = { 1,3,4 };
+    vector<int> v1 = { -9,3};
+    vector<int> v2 = { 5,7 };
     ListNode x1 = ListNode(v1[0]);
     ListNode * start1 = &x1;
     ListNode x2 = ListNode(v2[0]);
@@ -27,22 +27,22 @@ int main() {
     
     for (int i = 1; i < v1.size(); i++)
     {
-        ListNode x = ListNode(v1[i]);
-        curr->next = &x;
+        ListNode* x = new ListNode(v1[i]);
+        curr->next = x;
         curr = curr->next;
 
     }
     curr = start2;
     for (int i = 1; i < v2.size(); i++)
     {
-        ListNode x = ListNode(v2[i]);
-        curr->next = &x;
+        ListNode* x = new ListNode(v2[i]);
+        curr->next = x;
         curr = curr->next;
     }
 
     curr = mergeTwoLists(start1, start2);
     
-    //cout << insert(v, 2);
+    cout << "l";// insert(v, 2);
 
 	return 0;
 }
@@ -51,46 +51,72 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         return list2;
     }
     else if (list2 == nullptr) {
-        return list2;
+        return list1;
     }
 
     if (list1->val < list2->val) {
         ListNode* curr = list1->next;
         ListNode* prev = list1;
 
-        while (curr->next != nullptr) {
-            if (curr->val < list2->val) {
-                prev = curr;
-                curr = curr->next;
-            }
-            else {
-                prev->next = list2;
-                list2 = list2->next;
-                prev->next = curr;
-                prev = prev->next;
-                if (list2 == nullptr) {
-                    return list1;
+        if (curr != nullptr) {
+            while (curr->next != nullptr) {
+                if (curr->val < list2->val) {
+                    prev = curr;
+                    curr = curr->next;
                 }
-            }
-        }
-        while (curr != nullptr) {
-            if (curr->val < list2->val) {
-                prev = curr;
-                curr = curr->next;
-                break;
-            }
-            else {
-                prev->next = list2;
-                list2 = list2->next;
-                prev->next = curr;
-                prev = prev->next;
-                if (list2 == nullptr) {
-                    return list1;
+                else {
+                    prev->next = list2;
+                    list2 = list2->next;
+                    prev->next->next = curr;
+                    prev = prev->next;
+                    if (list2 == nullptr) {
+                        return list1;
+                    }
                 }
             }
 
+
+            while (list2->next != nullptr) {
+                if (curr->val < list2->val) {
+                    prev = curr;
+                    curr = curr->next;
+                    break;
+                }
+                else {
+                    prev->next = list2;
+                    list2 = list2->next;
+                    prev->next->next = curr;
+                    prev = prev->next;
+                    if (list2 == nullptr) {
+                        return list1;
+                    }
+                }
+
+            }
+            if (curr != nullptr) {
+
+                if (curr->val < list2->val) {
+                    prev = curr;
+                    curr = curr->next;
+
+                }
+                else {
+                    prev->next = list2;
+                    list2 = list2->next;
+                    prev->next->next = curr;
+                    prev = prev->next;
+                    if (list2 == nullptr) {
+                        return list1;
+                    }
+                }
+            }
+            prev->next = list2;
         }
-        curr->next = list2;
+        else {
+            prev->next = list2;
+        }
+
+
         return list1;
 
     }
@@ -99,46 +125,67 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode* curr = list2->next;
         ListNode* prev = list2;
 
-        while (curr->next != nullptr) {
-            if (curr->val < list2->val) {
-                prev = curr;
-                curr = curr->next;
-            }
-            else {
-                prev->next = list2;
-                list2 = list2->next;
-                prev->next = curr;
-                prev = prev->next;
-                if (list2 == nullptr) {
-                    return list1;
+        if (curr != nullptr) {
+            while (curr->next != nullptr) {
+                if (curr->val < list1->val) {
+                    prev = curr;
+                    curr = curr->next;
                 }
-            }
-        }
-
-        while (curr != nullptr) {
-            if (curr->val < list1->val) {
-                prev = curr;
-                //curr = curr->next;
-                break;
-            }
-            else {
-                prev->next = list1;
-                list1 = list1->next;
-                prev->next = curr;
-                prev = prev->next;
-                if (list1 == nullptr) {
-                    return list2;
+                else {
+                    prev->next = list1;
+                    list1 = list1->next;
+                    prev->next->next = curr;
+                    prev = prev->next;
+                    if (list1 == nullptr) {
+                        return list2;
+                    }
                 }
             }
 
+
+            while (list1->next != nullptr) {
+                if (curr->val < list1->val) {
+                    prev = curr;
+                    curr = curr->next;
+                    break;
+                }
+                else {
+                    prev->next = list1;
+                    list1 = list1->next;
+                    prev->next->next = curr;
+                    prev = prev->next;
+                    if (list1 == nullptr) {
+                        return list2;
+                    }
+                }
+
+            }
+            if (curr != nullptr) {
+                if (curr->val < list1->val) {
+                    prev = curr;
+                    curr = curr->next;
+
+                }
+                else {
+                    prev->next = list1;
+                    list1 = list1->next;
+                    prev->next->next = curr;
+                    prev = prev->next;
+                    if (list1 == nullptr) {
+                        return list2;
+                    }
+                }
+            }
+            
+            prev->next = list1;
         }
-        curr->next = list1;
+        else {
+            prev->next = list1;
+        }
+
+        
         return list2;
 
     }
-
-
-
-
 
 }
